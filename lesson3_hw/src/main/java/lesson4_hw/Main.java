161110fcbf16d36d555baf733549bbad052cf007
Sandor Ivanyo <139584544+SancosJak@users.noreply.github.com>
2024-04-11 22:07:49 +0200

    update

--- lesson3_hw/src/main/java/lesson4_hw/Main.java
+++ lesson3_hw/src/main/java/lesson4_hw/Main.java
@@ -14,7 +14,7 @@ public class Main {
         Wolf wolf = annotationContext.getBean(Wolf.class);
         Eagle eagle = annotationContext.getBean(Eagle.class);
 
-        System.out.println("Names of animals created using annotations:");
+        System.out.println("\u001B[34mNames (annotations): \u001B[0m");
         System.out.println("Lion: " + lion.getName());
         System.out.println("Wolf: " + wolf.getName());
         System.out.println("Eagle: " + eagle.getName());
@@ -26,7 +26,7 @@ public class Main {
         Wolf wolfXml = xmlContext.getBean(Wolf.class);
         Eagle eagleXml = xmlContext.getBean(Eagle.class);
 
-        System.out.println("\nNames of animals created using XML:");
+        System.out.println("\u001B[35mNames (XML): \u001B[0m");
         System.out.println("Lion: " + lionXml.getName());
         System.out.println("Wolf: " + wolfXml.getName());
         System.out.println("Eagle: " + eagleXml.getName());
