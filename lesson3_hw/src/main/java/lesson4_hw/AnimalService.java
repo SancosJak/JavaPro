16330aaa6fe58caf1aec0f7d34bbbe2c1010142a
Sandor Ivanyo <139584544+SancosJak@users.noreply.github.com>
2024-04-15 22:07:33 +0200

    update

--- lesson3_hw/src/main/java/lesson4_hw/AnimalService.java
+++ lesson3_hw/src/main/java/lesson4_hw/AnimalService.java
@@ -1,6 +1,7 @@
 package lesson4_hw;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -12,6 +13,7 @@ public class AnimalService {
     private List<Animal> animals;
 
     @Autowired
+    @Qualifier("lion")
     private Animal lion;
 
     public List<Animal> getAnimals() {
