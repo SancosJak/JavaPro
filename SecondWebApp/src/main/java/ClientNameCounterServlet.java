095be24cf520d75b1b22019279f0b6eaeeafdaa2
Sandor Ivanyo <139584544+SancosJak@users.noreply.github.com>
2024-04-19 12:01:09 +0200

    Create ClientNameCounterServlet.java

---
+++ SecondWebApp/src/main/java/ClientNameCounterServlet.java
@@ -0,0 +1,35 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/cnc")  //cnc - client name counter
+public class ClientNameCounterServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        HttpSession session = request.getSession();
+        Integer count = (Integer) session.getAttribute("count");
+
+        if (count == null) count = 0;
+
+        String clientName = request.getParameter("name");
+        count++;
+        session.setAttribute("count", count);
+
+        PrintWriter writer = response.getWriter();
+
+        writer.println("<html>");
+        writer.println("<head><title>Client count servlet</title></head>");
+        writer.println("<body>");
+        writer.println("<h1>Client Name: " + clientName + "</h1>");
+        writer.println("<h1>Count: " + count + "</h1>");
+        writer.println("</body>");
+        writer.println("</html>");
+
+        writer.close();
+    }
+}
