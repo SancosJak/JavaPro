0c90a86fdaa44e395a415ccc0e4d9b87fca5c23d
Sandor Ivanyo <139584544+SancosJak@users.noreply.github.com>
2024-05-06 17:19:12 +0200

    update

--- education_center/src/test/java/de/ait/EducationCenterApplicationTests.java
+++ education_center/src/test/java/de/ait/EducationCenterApplicationTests.java
@@ -1,13 +1,33 @@
 package de.ait;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-class EducationCenterApplicationTests {
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class EducationCenterApplicationTests {
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
-    void contextLoads() {
+    public void simple_test() throws Exception{
+        mockMvc.perform(get("/api/courses"))
+                .andDo(print());
+    }
+
+    @Test
+    public void return_list_of_courses_test() throws Exception{
+        mockMvc.perform(get("/api/courses"))
+                .andExpect(status().isOk());
     }
 
 }
